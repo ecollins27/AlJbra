433dcf4059ef2801ac49562dc9c47c5c2ac31f4c
ecollins27 <ehren.collins@gmail.com>
2023-08-01 15:07:05 -0400

    Updated README.md

--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,5 @@ public class Main {
     TODO:
      */
     public static void main(String[] args) {
-        Expression f = Fraction.valueOf(0.33,true);
-        Expression f2 = new Scalar(3).invert();
-        System.out.println(f.equals(f2));
     }
 }
