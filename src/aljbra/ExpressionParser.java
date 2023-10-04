3406b53d33e00ad776dfba4d2291a019d519814f
ecollins27 <ehren.collins@gmail.com>
2023-10-03 22:56:32 -0400

    exported as jar

--- src/aljbra/ExpressionParser.java
+++ src/aljbra/ExpressionParser.java
@@ -2,6 +2,8 @@ package aljbra;
 
 import aljbra.trig.Trig;
 
+import java.lang.reflect.InvocationTargetException;
+
 public final class ExpressionParser {
 
     public static Expression parse(String str) {
@@ -16,8 +18,8 @@ public final class ExpressionParser {
             return Product.parseProduct(str);
         } else if (Exponential.isExponential(str)){
             return Exponential.parseExponential(str);
-        } else if (Trig.isTrig(str)){
-            return Trig.parseTrig(str);
+        } else if (isTrig(str)){
+            return parseTrig(str);
         } else if (Log.isLog(str)){
             return Log.parseLog(str);
         } else if (Abs.isAbs(str)){
@@ -32,5 +34,31 @@ public final class ExpressionParser {
             return Variable.parseVariable(str);
         }
         throw new RuntimeException("Cannot convert " + str + " to Expression");
+    }
+
+    protected static boolean isTrig(String str){
+        String[] names = new String[]{"sin","cos","tan","asin","acos","atan"};
+        for (String name: names) {
+            if (str.length() < name.length() + 3) {
+                return false;
+            } else if (str.substring(0, name.length()).equals(name) && str.charAt(name.length()) == '(' && Expression.getMatchingDelimeter(str, name.length()) == str.length() - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected static Expression parseTrig(String str){
+        String name = str.substring(0,str.indexOf('('));
+        Expression operand = ExpressionParser.parse(str.substring(str.indexOf('(') + 1,str.length() - 1));
+        try {
+            return (Expression) Trig.class.getMethod(name,Expression.class).invoke(null,operand);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/aljbra/trig/Trig.java
+++ src/aljbra/trig/Trig.java
@@ -187,32 +187,6 @@ public abstract class Trig extends Expression {
         }
     }
 
-    protected static boolean isTrig(String str){
-        String[] names = new String[]{"sin","cos","tan","asin","acos","atan"};
-        for (String name: names) {
-            if (str.length() < name.length() + 3) {
-                return false;
-            } else if (str.substring(0, name.length()).equals(name) && str.charAt(name.length()) == '(' && getMatchingDelimeter(str, name.length()) == str.length() - 1) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    protected static Expression parseTrig(String str){
-        String name = str.substring(0,str.indexOf('('));
-        Expression operand = ExpressionParser.parse(str.substring(str.indexOf('(') + 1,str.length() - 1));
-        try {
-            return (Expression) Trig.class.getMethod(name,Expression.class).invoke(null,operand);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (InvocationTargetException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchMethodException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     static interface Function {
         double eval(double n);
     }
