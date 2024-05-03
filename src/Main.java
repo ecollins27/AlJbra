e81d6ab6f7f123e827a2f5dd5259d2bc7be4c41a
ecollins27 <ehren.collins@gmail.com>
2024-05-03 16:41:29 -0400

    Exported to jar

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Trig.atan(Decimal.ONE));
+        Variable x = new Variable('x');
+        System.out.println(x.sq().divide(x.multiply(Scalar.TWO)));
     }
 }
